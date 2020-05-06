393e2228d488c6af6dda871bc7a013a991d98bf6
jmoze <jonathan.moze@etu.u-bordeaux.fr>
2020-05-06 11:47:26 +0100

    debut phase variables

---
+++ Calculette_V3.0/test/testPackage/testCalculator.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testPackage;
+
+import static junit.framework.Assert.assertEquals;
+import my.calculator.Calculator;
+import my.calculator.EvaluationErrorException;
+import my.calculator.SyntaxErrorException;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ *
+ * @author jonat
+ */
+public class testCalculator {
+
+    @Test
+    public void testEvaluation() throws SyntaxErrorException, EvaluationErrorException {
+        Calculator calc = new Calculator();
+        assertEquals(123, calc.evaluation("  123 "));
+        assertEquals(4, calc.evaluation("2+2"));
+        assertEquals(1, calc.evaluation("421-20-400"));
+        assertEquals(6, calc.evaluation("2*3"));
+        assertEquals(2, calc.evaluation("7/3"));
+        assertFalse(calc.evaluation("23 + 45") == 26);
+        assertEquals(18, calc.evaluation("2*(4+5)"));
+        assertEquals(2, calc.evaluation("6/(2*3 - 3)"));
+        assertEquals(-12, calc.evaluation("3 * -(3+1)"));
+    }
+
+    @Test
+    public void testDivisionZero() throws SyntaxErrorException {
+        Calculator calc = new Calculator();
+        try {
+            calc.evaluation("1 + 2/0 + 3");
+            fail();
+        } catch (EvaluationErrorException ex) {
+        }
+    }
+}
